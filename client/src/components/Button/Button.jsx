ee98fe9ca7baf67cb6b37d4ad581cb21d91592c8
Jacob Heldt <jacobemil.heldt@gmail.com>
2023-10-02 15:31:10 -0400

    Improve Structure and add Comments

--- client/src/components/Button/Button.jsx
+++ client/src/components/Button/Button.jsx
@@ -1,16 +1,35 @@
 // Button.js
 
+// Importing necessary libraries and styles
 import React from 'react';
 import PropTypes from 'prop-types';
-import './Button.css'; // Import the CSS file
+import './Button.css'; // Styling for the Button component
 
-const Button = ({ children, variant, onClick }) => {
+
+/**
+ * Button Component
+ * 
+ * This component renders a button with customizable properties.
+ * It supports different variants and sizes to accommodate various use cases.
+ * 
+ * @param {Object} props - The properties object.
+ * @param {React.ReactNode} props.children - The content to be displayed inside the button.
+ * @param {string} [props.variant='primary'] - The variant of the button, e.g., 'outline' for an outlined button.
+ * @param {string} [props.size] - The size of the button, e.g., 'big' for a larger button.
+ * @param {Function} [props.onClick] - The callback to be invoked when the button is clicked.
+ * 
+ * @returns {React.Element} The rendered Button component.
+ */
+const Button = ({ children, variant, size, onClick }) => {
+  // Base class for all buttons
   let className = 'button';
 
-  if (variant === 'outline') {
-    className += ' button-outline';
-  }
+  // Modify class based on the variant and size props to apply different styles
+  if (variant === 'outline') className += ' button-outline';
+  if (size === 'big') className += ' button-big';
+  if (size === 'big-outline') className += ' button-big-outline';
 
+  // Render the button with the computed className and provided props
   return (
     <button className={className} onClick={onClick}>
       {children}
@@ -18,15 +37,19 @@ const Button = ({ children, variant, onClick }) => {
   );
 };
 
+// Define the types of props for better documentation and validation
 Button.propTypes = {
-  children: PropTypes.node.isRequired,
-  variant: PropTypes.string,
-  onClick: PropTypes.func,
+  children: PropTypes.node.isRequired, // Content of the button, can be text or another component
+  variant: PropTypes.string, // Variant of the button, e.g., 'outline'
+  size: PropTypes.string, // Size of the button, e.g., 'big'
+  onClick: PropTypes.func, // Callback for click event
 };
 
+// Define default values for optional props
 Button.defaultProps = {
-  variant: 'primary',
-  onClick: () => {},
+  variant: 'primary', // Default to primary button if no variant is provided
+  onClick: () => {}, // Default to no-op function if no onClick is provided
 };
 
+// Export the Button component for use in other files
 export default Button;
--- client/src/components/Hero/Hero.jsx
+++ client/src/components/Hero/Hero.jsx
@@ -1,10 +1,25 @@
+// Importing necessary libraries and components
 import { Link } from "react-router-dom";
 import Button from '../Button/Button';
-import './Hero.css';
+import './Hero.css'; // Importing styling specific to the Hero component
+import { FaChevronRight } from 'react-icons/fa';
 
+/**
+ * Hero Component
+ * 
+ * This component represents the hero section of a webpage, typically used
+ * on the landing page. It contains a title, a description, and a group of buttons.
+ * 
+ * The title has decorative SVG elements and the description emphasizes certain words
+ * with a primary text color. The buttons navigate to the waitlist page and have different
+ * styles and sizes.
+ * 
+ * @returns {React.Element} The rendered Hero component.
+ */
 const Hero = () => {
     return (
         <div className="hero-container">
+          {/* Title with decorative SVG elements */}
           <h1 className="title title-sm">
             The{" "}
             <span className="relative-span text-primary">
@@ -17,21 +32,24 @@ const Hero = () => {
               <span className="relative-span heading-font"> Collaborative</span>
             </span>
           </h1>
+          
+          {/* Description with emphasized words */}
           <p className="description">
-            Opening doors to <b>accessible</b>, <b>collaborative</b> and <b>engaging</b> learning experiences
+            Opening doors to <b className="primary-text-color">accessible</b>, <b className="primary-text-color">collaborative</b> and <b className="primary-text-color">engaging</b> learning experiences
           </p>
+          
+          {/* Group of buttons with different styles and sizes */}
           <div className="button-group">
-            <Link to="/signIn">
-              <Button>Join us</Button>
+            <Link to="/waitlist">
+            <Button size="big">Join Waitlist <FaChevronRight className="icon-inline" /></Button>
             </Link>
-            <Link to="/courses">
-              <Button variant="outline">
-                <span className="ml-3">Discover Our Courses</span>
-              </Button>
+            <Link to="/waitlist">
+              <Button variant="outline" size="big-outline">Learn More</Button>
             </Link>
           </div>
         </div>
-      );
+    );
 }
 
-export default Hero
+// Exporting the Hero component for use in other files
+export default Hero;
