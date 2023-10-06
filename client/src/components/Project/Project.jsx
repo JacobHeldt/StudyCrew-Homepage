f5eebaaba301a862c37cd707474f9520c0b343ba
Jacob Heldt <jacobemil.heldt@gmail.com>
2023-10-06 19:38:09 -0400

    Improve documentation

--- client/src/components/Project/Project.jsx
+++ client/src/components/Project/Project.jsx
@@ -1,24 +1,34 @@
-import React from 'react'
+import React from 'react';
 import './Project.css';
 import project_image from '../../assets/project-image.jpg';
 
+/**
+ * Project Component
+ * 
+ * This component displays information about the project, including:
+ * - A description of the project's mission and goals.
+ * - An image representing the project.
+ * - A subtitle overlay on the image with a quote from the founder.
+ */
 const Project = () => {
   return (
     <div className='project-main-div'>
+        {/* Left column containing project description */}
         <div className='project-left-column'>
             <div className='project-text'>
-                <h3 className='mission-header white-text-color'>OUR PROJECT</h3>
-                <h2 className='mission-header white-text-color slogan'>Transforming Education Together.</h2>
-                <p className='mission-text white-text-color'>
+                <h3 className='project-mission-header white-text-color'>OUR PROJECT</h3>
+                <h2 className='project-mission-title white-text-color slogan'>Transforming Education Together.</h2>
+                <p className='project-mission-text white-text-color'>
                 Dedicated to transforming the educational landscape, our platform prioritizes accessibility, collaboration, and engagement. 
                 We are committed to ensure quality education is in everyone’s reach. Together, we’re constructing a platform that emphasizes collaborative learning. 
                 Embracing the spirit of collective knowledge and democratizing learning, our project is open-source, ensuring continuous innovation and community-driven enhancements.
                 </p>
             </div>
         </div>
+
+        {/* Right column containing project image and subtitle */}
         <div className='project-right-column'>
-            <img src={project_image} className='project-image'>
-            </img>
+            <img src={project_image} className='project-image' alt="Project representation" />
             <div className="project-image-subtitle">
               <div>
               “Every student deserves a dynamic and engaging educational experience. Let's build it together.”
@@ -29,7 +39,7 @@ const Project = () => {
             </div>
         </div>
     </div>
-  )
+  );
 }
 
-export default Project
+export default Project;
