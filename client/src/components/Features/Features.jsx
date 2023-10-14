9b5ec8e4882c80f38cebf5f0e33c009a5072358c
Jacob Heldt <jacobemil.heldt@gmail.com>
2023-10-14 08:19:31 -0600

    Resolve netlify build error

--- client/src/components/Features/Features.jsx
+++ client/src/components/Features/Features.jsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState, useEffect } from 'react';
+import React, { useRef, useState, useEffect, useCallback } from 'react';
 import "./Features.css";
 import { BsArrowRightShort, BsArrowLeftShort } from "react-icons/bs";
 
@@ -76,7 +76,7 @@ function Features() {
   }
 
   // Function to reset values on window resize
-  const reSetValue = () => {
+  const resetValue = useCallback(() => {
     if (window.innerWidth < 521) {
       setLimit(6);
     } else if (window.innerWidth < 861) {
@@ -87,14 +87,14 @@ function Features() {
     setCardWidth(0);
     setIndex(1);
     accordian("stage-1")
-  }
+  }, []);
   
-  useEffect(()=>{
-    window.addEventListener("resize", resetValue)
+  useEffect(() => {
+    window.addEventListener("resize", resetValue);
     return () => {
-      window.removeEventListener("resize", resetValue)
-    }
-  }, [resetValue])
+      window.removeEventListener("resize", resetValue);
+    };
+  }, [resetValue]);
 
   return (
 
