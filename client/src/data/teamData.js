e47ed4e7da72be046119b99833f346ea2fb853b6
Jacob Heldt <jacobemil.heldt@gmail.com>
2023-10-22 16:01:50 -0400

    Add team members
    - Create teamData.js file that holds team members
    - Import svg avatars

---
+++ client/src/data/teamData.js
@@ -0,0 +1,39 @@
+// teamData.js
+
+import jacob from "../assets/avatars/jacob.svg";
+import jona from "../assets/avatars/jona.svg";
+import sugumaran from "../assets/avatars/sugumaran.svg";
+import xavier from "../assets/avatars/xavier.svg";
+import nicholas from "../assets/avatars/nicholas.svg";
+// ... import other avatars
+
+const teamMembers = [
+  {
+    name: "Jacob",
+    role: "Founder",
+    avatar: jacob
+  },
+  {
+    name: "Jona",
+    role: "Marketing",
+    avatar: jona
+  },
+  {
+    name: "Sugumaran",
+    role: "Development",
+    avatar: sugumaran
+  },
+  {
+    name: "Xavier",
+    role: "Product Management",
+    avatar: xavier
+  },
+  {
+    name: "Nicholas",
+    role: "Marketing",
+    avatar: nicholas
+  }
+  // ... other team members
+];
+
+export default teamMembers;
