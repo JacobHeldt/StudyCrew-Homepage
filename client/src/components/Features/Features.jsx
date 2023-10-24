d2bd1d95375387c9b27bcc5956a0177f31a0f80d
Jacob Heldt <jacobemil.heldt@gmail.com>
2023-10-24 16:16:36 -0400

    Adjust scrolling on features section

--- client/src/components/Features/Features.jsx
+++ client/src/components/Features/Features.jsx
@@ -9,44 +9,57 @@ function Features() {
   const [cardWidth, setCardWidth] = useState(0);
   const [index, setIndex] = useState(1);
   const [limit, setLimit] = useState(6);
+  const [currentStage, setCurrentStage] = useState('stage-1');
 
-  // Function to scroll left
+  // Function to handle scrolling left in the carousel
   const scrollLeft = () => {
     const cardWidth = cardsContainer.current.firstChild.offsetWidth;
+    let scrollValue = cardWidth + 322;  // default value
+
+    if (currentStage === 'stage-3') {
+      scrollValue = cardWidth + 30;  // adjust for stage 3
+    }
+
     if (index > 1) {
-        setCardWidth(prevCardWidth => prevCardWidth + cardWidth + 324); 
-        setIndex(prevIndex => prevIndex - 1);
+      setCardWidth(prevCardWidth => prevCardWidth + scrollValue); 
+      setIndex(prevIndex => prevIndex - 1);
     }
   };
 
-  // Function to scroll right
+  // Function to handle scrolling right in the carousel
   const scrollRight = () => {
     const cardWidth = cardsContainer.current.firstChild.offsetWidth;
+    let scrollValue = cardWidth + 322;  // default value
+
+    if (currentStage === 'stage-3') {
+      scrollValue = cardWidth + 30;  // adjust for stage 3
+    }
+
     if (limit > index) {
-        setCardWidth(prevCardWidth => prevCardWidth - cardWidth - 324); 
-        setIndex(prevIndex => prevIndex + 1);
+      setCardWidth(prevCardWidth => prevCardWidth - scrollValue); 
+      setIndex(prevIndex => prevIndex + 1);
     }
   };
 
-
-  // Function to handle click on stage for larger screens
+  // Handle clicks on stages for larger screens
   const accordian = (id) => {
+    setCurrentStage(id);
+    setIndex(1);            
+    setCardWidth(0); 
+
     let ele = owlCarousel.current.children;
     switch (id) {
       case "stage-1":
-        // Show the appropriate content for stage 1
         ele[1].style.display = "grid";
         ele[3].style.display = "none";
         ele[5].style.display = "none";
         break;
       case "stage-2":
-        // Show the appropriate content for stage 2
         ele[1].style.display = "none";
         ele[3].style.display = "grid";
         ele[5].style.display = "none";
         break;
       case "stage-3":
-        // Show the appropriate content for stage 3
         ele[1].style.display = "none";
         ele[3].style.display = "none";
         ele[5].style.display = "grid";
@@ -56,20 +69,18 @@ function Features() {
         break;
     }
   }
-  // Function to handle click on stage for smaller screens (mobile)
+
+  // Handle clicks on stages for mobile screens
   const mobileAccordian = (id) => {
     let ele = owlCarousel.current.children;
     switch (id) {
       case "stage-1":
-        // Toggle the display of content for stage 1
         ele[1].style.display = ele[1].style.display === "grid" ? "none" : "grid";
         break;
       case "stage-2":
-        // Toggle the display of content for stage 2
         ele[3].style.display = ele[3].style.display === "grid" ? "none" : "grid";
         break;
       case "stage-3":
-        // Toggle the display of content for stage 3
         ele[5].style.display = ele[5].style.display === "grid" ? "none" : "grid";
         break;
       default:
@@ -78,40 +89,30 @@ function Features() {
     }
   }
 
+  // On component mount
   useEffect(() => {
+    resetValue();
     accordian("stage-1"); 
   }, []);
 
-  // Function to reset values on window resize
+  // Reset carousel values on window resize
   const resetValue = useCallback(() => {
-    if (window.innerWidth < 521) {
-      setLimit(6);
-    } else if (window.innerWidth < 861) {
-      setLimit(5);
+    if (currentStage === 'stage-3') {
+      setLimit(3);
     } else {
-      setLimit(4);
+      setLimit(6);
     }
     setCardWidth(0);
     setIndex(1);
-    accordian("stage-1")
-  }, []);
-  
-  useEffect(() => {
-    window.addEventListener("resize", resetValue);
-    return () => {
-      window.removeEventListener("resize", resetValue);
-    };
-  }, [resetValue]);
+  }, [currentStage]);
 
   return (
-
     <div className="features">
       <div className="features-head">
         <h2>Our <span>Features</span></h2>
         <p className="des">Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo at a dolor voluptatum.</p>
       </div>
       <div className="container">
-        {/* Stages for larger screens */}
         <div className="stages">
           {stages.map(stage => (
             <div key={stage.id} className="stage" onClick={() => accordian(stage.id)}>
@@ -121,7 +122,6 @@ function Features() {
           ))}
         </div>
 
-        {/* Owl carousel wrapper */}
         <div className="owl-carousel-wrapper" ref={owlCarousel}>
           {stages.map(stage => (
             <React.Fragment key={stage.id}>
@@ -138,25 +138,19 @@ function Features() {
                       <p className="card-description">{card.description}</p>
                     </div>
                   ))}
+                </div>
+
+                <div className="arrow-icons">
+                  <BsArrowLeftShort onClick={scrollLeft} size={50} className="arrow-left" />
+                  <BsArrowRightShort size={50} onClick={scrollRight} className="arrow-right" />
+                </div>
               </div>
-              {/* Arrow icons for carousel */}
-              <div className="arrow-icons">
-                <BsArrowLeftShort onClick={scrollLeft} size={50} className="arrow-left" />
-                <BsArrowRightShort size={50} onClick={scrollRight} className="arrow-right" />
-           
-            </div>
-          </div>
-
-          </React.Fragment>
-
+            </React.Fragment>
           ))}
-
         </div>
       </div>
     </div>
   );
 }
-
-
 
 export default Features;
