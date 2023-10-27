e813e7f48aaa46ea415a0c06d303777915c7776c
ELCOT <marvelmaran07@gmail.com>
2023-10-27 14:46:31 +0530

    faqs section

--- client/src/App.js
+++ client/src/App.js
@@ -1,146 +1,68 @@
 // React specific imports
-import React, { useEffect, useState } from "react";
+import React from "react";
+// import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from "react-router-dom";
 
 // Imports for styling
+// import logo from './logo.svg';
 import "./App.css";
 import "./index.css";
 
-// Importing individual components
+// Import components
 import Header from './components/Header/Header.jsx';
 import Hero from './components/Hero/Hero.jsx';
 import Mission from './components/Mission/Mission.jsx';
 import Features from './components/Features/Features.jsx';
 import Project from './components/Project/Project.jsx';
-import SignUp from './components/SignUp/Signup.jsx';
-import Team from './components/Team/Team.jsx';
-import Footer from './components/Footer/Footer.jsx';
 
-import withScrollFadeIn from './scripts/useFadeIn';  
-const FadingHero = withScrollFadeIn(Hero);
-const FadingMission = withScrollFadeIn(Mission);
-const FadingFeatures = withScrollFadeIn(Features);
-const FadingProject = withScrollFadeIn(Project);
-const FadingSignUp = withScrollFadeIn(SignUp);
-const FadingTeam = withScrollFadeIn(Team);
+
+import Team from './components/Team/Team.jsx';
+
+import Footer from './components/Footer/Footer';
+import Faqs from "./components/FAQs/Faqs";
+
 
 function App() {
-  // State for the active page section
-  const [activePage, setActivePage] = useState('');
-
-  // Refs to target elements for the IntersectionObserver
   const missionRef = React.useRef(null);
   const featuresRef = React.useRef(null);
   const projectRef = React.useRef(null);
   const teamRef = React.useRef(null);
-  const signupRef = React.useRef(null);
 
-  useEffect(() => {
-    // Cached ref nodes
-    const missionNode = missionRef.current;
-    const featuresNode = featuresRef.current;
-    const projectNode = projectRef.current;
-    const teamNode = teamRef.current;
-    const signupNode = signupRef.current;
-
-    // Callback to handle what happens when the elements intersect
-    const callback = (entries) => {
-      entries.forEach(entry => {
-        if (entry.isIntersecting) {
-          // Setting the active page based on the intersecting component
-          switch (entry.target.className) {
-            case 'mission-component':
-              setActivePage('mission');
-              break;
-            case 'features-component':
-              setActivePage('features');
-              break;
-            case 'project-component':
-              setActivePage('project');
-              break;
-            case 'empower-component':
-              setActivePage('signup');
-              break;
-            case 'team-component':
-              setActivePage('team');
-              break;
-            default:
-              break;
-          }
-        }
-      });
-    };
-
-    // Options for IntersectionObserver
-    const options = {
-      rootMargin: '-50% 0px -50% 0px',  // The callback is triggered when half of the target is visible.
-      threshold: 0
-    };
-
-    // Setting up the IntersectionObserver
-    const observer = new IntersectionObserver(callback, options);
-
-    // Observing the refs
-    if (missionNode) observer.observe(missionNode);
-    if (featuresNode) observer.observe(featuresNode);
-    if (projectNode) observer.observe(projectNode);
-    if (teamNode) observer.observe(teamNode);
-    if (signupNode) observer.observe(signupNode);
-
-    // Cleanup function to unobserve the refs
-    return () => {
-      if (missionNode) observer.unobserve(missionNode);
-      if (featuresNode) observer.unobserve(featuresNode);
-      if (projectNode) observer.unobserve(projectNode);
-      if (teamNode) observer.unobserve(teamNode);
-      if (signupNode) observer.unobserve(signupNode);
-    };
-  }, []);
-
-  // Component render
   return (
     <Router>
-      {/* Header with page navigation props */}
-      <Header 
+      < Header 
         missionRef={missionRef} 
         featuresRef={featuresRef} 
         projectRef={projectRef}
         teamRef={teamRef}
-        signupRef={signupRef}
-        activePage={activePage}
-        setActivePage={setActivePage}
       />
 
-      {/* Various site sections */}
-      <FadingHero id="hero" delay={100} />
+      < Hero />
 
       <div className='mission-component' ref={missionRef}>
-        <FadingMission id="mission" />
+      < Mission/>
       </div>
 
       <div className='features-component' ref={featuresRef}>
-        <FadingFeatures id="features" />
+      < Features />
       </div>
 
       <div className='project-component' ref={projectRef}>
-        <FadingProject id="project" />
+      < Project />
       </div>
 
-      <div className="empower-component" ref={signupRef}>
-        <FadingSignUp id="sign-up" />
-      </div>
 
       <div className='team-component' ref={teamRef}>
-        <FadingTeam id="team" />
+        <Team />
       </div>
-
-      {/* Footer */}
+     <div className="faqs-component">
+        <Faqs/>
+     </div>
       <div className="footer-component">
-        <Footer />
+      <Footer/>
       </div>
-    </Router>
+    </Router> 
   );
 }
 
-// Exporting the App component
 export default App;
