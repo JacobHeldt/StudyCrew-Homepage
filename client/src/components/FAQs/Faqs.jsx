00fb987eda53055c6aae1a7db7a0ef08c3b10685
Jacob Heldt <jacobemil.heldt@gmail.com>
2023-11-09 21:10:00 -0500

    Add correct FAQs

--- client/src/components/FAQs/Faqs.jsx
+++ client/src/components/FAQs/Faqs.jsx
@@ -1,35 +1,22 @@
 import React from "react";
 import "./Faqs.css";
+import faqsData from "../../data/faqsData";
 
 function Faqs() {
     return(
         <div className="faqs">
-            <h1 className="faqs-head"> <span>faq</span>s</h1>
+            <h2 className="faqs-heading"><span>FAQ</span>s</h2>
             <p className="faqs-des">We are here to help you with any question you have</p>
             <div className="questions-list">
-                <details className="question">
-                    <summary className="question-head">Can I transfer earned credits to another institution?</summary>
-                    <p className="question-des">While we strive to provide a quality educational experience, the acceptance of transfer credits is ultimately at the discretion of the institution you plan to attend. We recommend early and thorough communication with the admissions or registrar's office at that institution to ensure a smooth credit transfer process. Additionally, maintaining good academic performance in our program can enhance your chances of successful credit transfer.</p>
-                </details>
-                <details className="question">
-                    <summary className="question-head">How will tutoring be offered?</summary>
-                    <p className="question-des">While we strive to provide a quality educational experience, the acceptance of transfer credits is ultimately at the discretion of the institution you plan to attend. We recommend early and thorough communication with the admissions or registrar's office at that institution to ensure a smooth credit transfer process. Additionally, maintaining good academic performance in our program can enhance your chances of successful credit transfer.</p>
-                </details>
-                <details className="question">
-                    <summary className="question-head">How can I apply for a tutoring role?</summary>
-                    <p className="question-des">While we strive to provide a quality educational experience, the acceptance of transfer credits is ultimately at the discretion of the institution you plan to attend. We recommend early and thorough communication with the admissions or registrar's office at that institution to ensure a smooth credit transfer process. Additionally, maintaining good academic performance in our program can enhance your chances of successful credit transfer.</p>
-                </details>
-                <details className="question">
-                    <summary className="question-head">Will there be a mobile app?</summary>
-                    <p className="question-des">While we strive to provide a quality educational experience, the acceptance of transfer credits is ultimately at the discretion of the institution you plan to attend. We recommend early and thorough communication with the admissions or registrar's office at that institution to ensure a smooth credit transfer process. Additionally, maintaining good academic performance in our program can enhance your chances of successful credit transfer.</p>
-                </details>
-                <details className="question">
-                    <summary className="question-head">How do you handle user-feedback and suggestions?</summary>
-                    <p className="question-des">While we strive to provide a quality educational experience, the acceptance of transfer credits is ultimately at the discretion of the institution you plan to attend. We recommend early and thorough communication with the admissions or registrar's office at that institution to ensure a smooth credit transfer process. Additionally, maintaining good academic performance in our program can enhance your chances of successful credit transfer.</p>
-                </details>
-                </div>
-       </div> 
-    )
+                {faqsData.map((faq, index) => (
+                    <details className="question" key={index}>
+                        <summary className="question-head">{faq.question}</summary>
+                        <div className="question-des">{faq.answer}</div>
+                    </details>
+                ))}
+            </div>
+        </div>
+    );
 }
 
-export default Faqs
+export default Faqs;
